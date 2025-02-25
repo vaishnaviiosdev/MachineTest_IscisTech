c7c0eba7bb4c949757978fafdab49c4e812f3345
vaishnavichillar <vaishnavi.jothikumar@chillarcards.com>
2025-02-25 20:19:28 +0530

    Initial Commit

---
+++ MachineTest_IscisTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineTest_IscisTech
+//
+//  Created by Neshwa on 25/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MachineTest_IscisTechUITests/MachineTest_IscisTechUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MachineTest_IscisTechUITestsLaunchTests.swift
+//  MachineTest_IscisTechUITests
+//
+//  Created by Neshwa on 25/02/25.
+//
+
+import XCTest
+
+final class MachineTest_IscisTechUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
